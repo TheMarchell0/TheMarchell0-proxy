0afff713f9d5882ac9482041506288161d68d66c
unknown <TheMarchell0@yandex.ru>
2023-09-29 18:29:22 +0600

    try to fix promo nativity

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const googleScriptUrl = 'https://script.google.com/macros/s/AKfycbyEml6rBMVdGc0g33nqaXea-UDabkhvjvhfClK87mFW7g6KbuiOWMInulRf1ikwuddJaw/exec';
-
 app.post('/proxy', async (req, res) => {
     try {
+        let googleScriptUrl;
+        if(req.body.type === "Заявка"){
+            googleScriptUrl = "https://script.google.com/macros/s/AKfycbyoPqNg5XfUPhO0z3tNMw7YmSfBSMF-rm-i6or2pROfjTpoEoMtOwgp6qXDj2j9prFiWg/exec";
+        }
+        else if(req.body.type === "Заявка на консультацию"){
+            googleScriptUrl = "https://script.google.com/macros/s/AKfycbyE7f1e6d7YBtXk_udscg48N09q15ZQ49_bHSsaHxqE2KJ7zTiOvEVRdzrM7yc7Vjx28A/exec";
+        }
+        else{
+            googleScriptUrl = 'https://script.google.com/macros/s/AKfycbyEml6rBMVdGc0g33nqaXea-UDabkhvjvhfClK87mFW7g6KbuiOWMInulRf1ikwuddJaw/exec';
+        }
+
         const response = await axios.post(googleScriptUrl, req.body);
         res.send(response.data);
     } catch (error) {
@@ -24,6 +33,7 @@ app.post('/proxy', async (req, res) => {
         res.status(500).send('Error with POST request: ' + error.message);
     }
 });
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Proxy server listening on port ${port}`));
