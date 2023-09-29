787f0bd86bfc92d24c5786218f903a34c6225b18
unknown <TheMarchell0@yandex.ru>
2023-09-29 19:37:37 +0600

    try to fix promo nativity

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(express.urlencoded({extended: true}));
 app.post('/proxy', async (req, res) => {
     let googleScriptUrl;
     if(req.body.question && (req.headers.origin === 'http://localhost:3000')){
-        googleScriptUrl = "https://script.google.com/macros/s/AKfycbzpiLI1HuXmEimE15WaiCWty9J-dPE_u2xCN1rivRPe6SMf48oSRXZJWReyt7H6W5h9wQ/exec";
+        googleScriptUrl = "https://script.google.com/macros/s/AKfycbwDGDGJch0McpuMMotfZpN_gLqTo66etvf2a0TihsPoLJoOUpo87fS6CxqDhwQgis0d2A/exec";
     }
     else if(req.headers.origin === 'http://localhost:3000'){
-        googleScriptUrl = "https://script.google.com/macros/s/AKfycbzHDWNSuH2xyMbOqrNNa9UjMPU5YLlS_LxufhdsBune-CqAJHQHSCZU5DgKptNfdzli/exec";
+        googleScriptUrl = "https://script.google.com/macros/s/AKfycbxlBOHZhI7jLT-MHKgihYTVjQioiey89IkCEtQAZMmXL0qz_ZVOtPehhHfjeI2Tn1o0/exec";
     }
     else{
         googleScriptUrl = 'https://script.google.com/macros/s/AKfycbyEml6rBMVdGc0g33nqaXea-UDabkhvjvhfClK87mFW7g6KbuiOWMInulRf1ikwuddJaw/exec';
